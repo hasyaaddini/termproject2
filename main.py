c3c67300f0aca37f4667db64e81d740df97f1acd
hasyaaddini <hasyaaddini@gmail.com>
2025-11-28 23:09:54 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+class Cat:
+    def meow(self):
+        print("I love chicken")
+
+def main():
+    c = Cat()
+    c.meow()
+
+if __name__ == "__main__":
+    main()
+=======
+class Cat:
+    def meow(self):
+        print("I love chicken")
+
+def main():
+    c = Cat()
+    c.meow()
+
+if __name__ == "__main__":
+    main()
