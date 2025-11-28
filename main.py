f245a29fe03b2b725528dd3a5d922b2155a393f7
hasyaaddini <hasyaaddini@gmail.com>
2025-11-28 23:49:43 +0900

    cleared smth

--- main.py
+++ main.py
@@ -8,14 +8,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-class Cat:
-    def meow(self):
-        print("I love chicken")
-
-def main():
-    c = Cat()
-    c.meow()
-
-if __name__ == "__main__":
-    main()
